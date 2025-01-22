d50993489e3870988fff765bd4bfe6082c1c28ff
Skog <krisskog@gmail.com>
2025-01-22 22:04:46 +0100

    Base main menu screen, some theme stuff. Added temporary fonts.

--- source/scenes/ui/menus/main_menu.gd
+++ source/scenes/ui/menus/main_menu.gd
@@ -1,10 +1,17 @@
-extends Node
+extends Control
+
+signal start_game()
+signal exit_game()
+
+func _on_start_game_button_pressed() -> void:
+	start_game.emit()
+	hide()
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
